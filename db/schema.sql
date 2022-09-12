90a86742d8365f9812000eb5f5ef25050eb85194
Austin Stoppenhagen <arstop98@gmail.com>
2022-09-12 11:23:51 -0500

    added schemas

--- db/schema.sql
+++ db/schema.sql
@@ -5,3 +5,11 @@ CREATE TABLE department (
     id Int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) UNIQUE NOT NULL
 );
+
+CREATE TABLE role(
+    -- needs id, 
+);
+
+CREATE TABLE employee(
+    -- needs id, first name, last name, role_id(fk), 
+);
---
+++ index.js
@@ -0,0 +1,10 @@
+const inquirer = require('inquirer');
+
+require('console.table');
+
+async function something() {
+    inquirer
+    .prompt([
+
+    ])
+};
