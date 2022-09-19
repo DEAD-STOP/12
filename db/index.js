00faeb461d800be3b5f18c75336ed025bb9cf9dc
DEAD-STOP <arstop98@gmail.com>
2022-09-18 20:21:56 -0500

    index.js

--- db/index.js
+++ db/index.js
@@ -13,20 +13,6 @@ class Company {
   viewEmployeeTable() {
     return this.connection.promise().query("SELECT * FROM EMPLOYEE");
   }
-  insertDept(input) {
-    return this.connection
-      .promise()
-      .query("INSERT INTO DEPARTMENT dept_name VALUES (?)", input);
-  }
-  insertRole(input) {
-    return this.connection
-      .promise()
-      .query("INSERT INTO role (title) VALUES (?)", input);
-  }
-  insertEmployee(input) {
-    return this.connection
-      .promise()
-      .query("INSERT INTO employee (first_name) VALUES (?)", input);
-  }
+
 }
 module.exports = new Company(connection);
